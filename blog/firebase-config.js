47d1c9cd20d234a6c2b8a6eb80d3c4aa3573bdc1
Logon Aluno <logonrmlocal@fiap.com.br>
2023-06-13 08:42:31 -0300

    Firebase

---
+++ blog/firebase-config.js
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyBdDx0OgYXAbbafNZGRwJWGwHjKX0Ca5Ho",
+  authDomain: "blog2mia-cfd63.firebaseapp.com",
+  projectId: "blog2mia-cfd63",
+  storageBucket: "blog2mia-cfd63.appspot.com",
+  messagingSenderId: "1049563495602",
+  appId: "1:1049563495602:web:f9eee76078a23ed4271dab"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
